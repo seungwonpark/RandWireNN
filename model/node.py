db7ddf640ce7962607246a2b6d08d22b98f1af33
Seungwon Park <yyyyy@snu.ac.kr>
2019-04-10 05:27:54 +0000

    fix: resolve #2

--- model/node.py
+++ model/node.py
@@ -8,13 +8,18 @@ from .sep_conv import SeparableConv2d
 class NodeOp(nn.Module):
     def __init__(self, in_degree, in_channel, out_channel, stride):
         super(NodeOp, self).__init__()
-        self.agg_weight = nn.Parameter(torch.zeros(in_degree, requires_grad=True))
+        self.single = (in_degree == 1)
+        if not self.single:
+            self.agg_weight = nn.Parameter(torch.zeros(in_degree, requires_grad=True))
         self.conv = SeparableConv2d(in_channel, out_channel, kernel_size=3, padding=1, stride=stride)
         self.bn = nn.BatchNorm2d(out_channel)
 
     def forward(self, y):
         # y: [B, C, N, M, in_degree]
-        y = torch.matmul(y, torch.sigmoid(self.agg_weight)) # [B, C, N, M]
+        if self.single:
+            y = y.squeeze(-1)
+        else:
+            y = torch.matmul(y, torch.sigmoid(self.agg_weight)) # [B, C, N, M]
         y = F.relu(y) # [B, C, N, M]
         y = self.conv(y) # [B, C_out, N, M]
         y = self.bn(y) # [B, C_out, N, M]
